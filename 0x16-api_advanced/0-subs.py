62e398fbf58cb7609a454c51b3f7bee9c33cf13f
meryemouirem <meryemouirem94@gmail.com>
2024-08-13 05:22:07 -0700

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
 """
-this doc for module
+Script that queries subscribers on a given Reddit subreddit.
 """
-import requests
 
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+import requests
 
 
 def number_of_subscribers(subreddit):
-    """method doc"""
+    """Return the total number of subscribers on a given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()
-        return data["data"]["subscribers"]
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
         return 0
